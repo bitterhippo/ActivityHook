f6d98418bd6ce4b24dbf35d8a74928915d97102c
neror <bitterhippo@gmail.com>
2025-09-19 14:33:47 -0700

    configures back end for dot env and better build scripts

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,12 @@
 import express from "express";
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const username = process.env.GITHUB_USERNAME;
+const token = process.env.GITHUB_TOKEN;
+
+console.log("Loaded username:", username);
 
 const app = express();
 const PORT = process.env.PORT || 4000;
