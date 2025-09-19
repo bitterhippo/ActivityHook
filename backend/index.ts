2b44eefab366e898608f9b70c29d9e060d393c25
neror <bitterhippo@gmail.com>
2025-09-19 15:10:01 -0700

    adds token and proper request access to github

--- backend/index.ts
+++ backend/index.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-const username = process.env.GITHUB_USERNAME;
+const user = process.env.GITHUB_USERNAME;
 const token = process.env.GITHUB_TOKEN;
 
-console.log("Loaded username:", username);
+console.log("Loaded username:", user);
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,9 +14,14 @@ const PORT = process.env.PORT || 4000;
 // Example endpoint to pull GitHub activity
 app.get("/activity", async (req, res) => {
   try {
-    const username = "bitterhippo"; // replace with your GitHub username
+    const username = user;
     const response = await axios.get(
-      `https://api.github.com/users/${username}/events/public`
+      `https://api.github.com/users/${username}/events/public`,
+      {
+        headers: {
+          Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        },
+      }
     );
     res.json(response.data);
   } catch (err) {
