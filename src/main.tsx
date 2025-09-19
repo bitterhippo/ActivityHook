e0b47fa30c27c7ee28fe6cd6eb120fafff031c0f
neror <bitterhippo@gmail.com>
2025-09-19 01:31:32 -0700

    corrects errors regarding formating of tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,8 @@ import App from "./App";
 const container = document.getElementById("root");
 if (!container) throw new Error("Root container not found");
 
+// createRoot expects a container
 const root = createRoot(container);
+
+// render expects a ReactNode
 root.render(<App />);
